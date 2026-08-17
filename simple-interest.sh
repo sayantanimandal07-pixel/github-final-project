90fd62d24013a4d37e3462a56c29321b0a3a21b3
sayantanimandal07-pixel <sayantanimandal07@gmail.com>
2026-08-17 08:07:44 +0530

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -6,6 +6,6 @@ read -p "Enter Principal Amount: " principal
 read -p "Enter Rate of Interest: " rate
 read -p "Enter Time (in years): " time
 
-simple_interest=$(($principal * $rate * $time) / 100))
+simple_interest=$((principal * rate * time / 100))
 
 echo "Simple Interest = $simple_interest"
